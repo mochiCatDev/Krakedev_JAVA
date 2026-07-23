146174c02c577c4011af0bb69f772def9c9be2b1
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-23 17:27:59 -0500

    feat: crear metodo promediar en el documento Calculadora
    el metodo promediar recibe 3 parametros de tipo double, se suma los tres parametros y se divide sobre 3, retornar el resultado de tipo double

--- Clases/src/com/krakedev/Calculadora.java
+++ Clases/src/com/krakedev/Calculadora.java
@@ -10,4 +10,9 @@ public class Calculadora {
         double resultado = dividiendo / divisor;
         return resultado;
     };
+
+    public double promediar(double valor1, double valor2, double valor3) {
+        double resultado = (valor1 + valor2 + valor3)/3;
+        return resultado;
+    };
 }
