66fc3de9d635f9ecc71079b33b36d0aa1fe46699
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-23 17:49:51 -0500

    test: validar métodos principales de Calculadora
    Se verificó el correcto funcionamiento de multiplicar, dividir, promediar y mostrarResultado (4/4 tests pasados).

---
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,22 @@
+package com.krakedev;
+
+public class TestCalculadora {
+    public static void main(String[] args) {
+        Calculadora c1 = new Calculadora();
+
+        double r1, r2, r3;
+
+        r1 = c1.multiplicar(10, 5);
+        r2 = c1.dividir(10, 2);
+        r3 = c1.promediar(10, 8, 9);
+
+        // Ver resultados para verificar si todo esta bien
+        System.out.println("Resultado de la multiplicación entre 10 y 5: %.2f".formatted(r1));
+        System.out.println("Resultado de la división entre 10 y 2: %.2f".formatted(r2));
+        System.out.println("Resultado de promediar 10, 8 y 9: %.2f".formatted(r3));
+
+        // Funcion que no retorna nada, ejecuta un mensaje por consola
+        System.out.print("=== Mensaje del metodo mostrarResultado() ===\n");
+        c1.mostrarResultado();
+    }
+}
