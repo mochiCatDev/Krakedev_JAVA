c9646422d2d9724d60e19a335da3c93d49151db1
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-26 23:54:21 -0500

    feat: crear archivos Cuadrado.java y TestCuadrado.java del repositorio Krakedev_Figuras del usuario @mochiCatDev

---
+++ Clases/src/com/krakedev/Cuadrado.java
@@ -0,0 +1,21 @@
+package com.krakedev;
+
+public class Cuadrado {
+    private int lado;
+
+    // getter
+    public int getLado() { return lado; }
+
+    // setter
+    public void setLado(int lado) { this.lado = lado; }
+
+    public double calcularArea() {
+        double resultado = Math.pow(lado, 2);
+        return resultado;
+    }
+
+    public double calcularPerimetro() {
+        double resultado = lado * 4;
+        return resultado;
+    }
+}
