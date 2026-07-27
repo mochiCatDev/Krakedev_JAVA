f8b62eeede06eb62b6e0fda8ffa24276600a5aa5
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-27 00:00:46 -0500

    feat: crear archivos Rectangulo.java y TestRectangulo.java del repositorio Krakedev_Figuras del usuario @mochiCatDev

---
+++ Clases/src/com/krakedev/Rectangulo.java
@@ -0,0 +1,18 @@
+package com.krakedev;
+
+public class Rectangulo {
+    private double base, altura;
+
+    // getter
+    public double getBase() { return base; }
+    public double getAltura() { return altura; }
+
+    // setter
+    public void setBase(double base) { this.base = base; }
+    public void setAltura(double altura) { this.altura = altura; }
+
+    public double calcularPerimetro() {
+        double perimetro = 2 * base + 2 * altura;
+        return perimetro;
+    }
+}
